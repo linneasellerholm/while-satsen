588761a3ace4278df9a947b56aa22b1f3681dbb6
linneasellerholm <linneasm@vgy.se>
2016-09-23 15:11:27 +0200

    nya övningar

--- while-satsen/Program.cs
+++ while-satsen/Program.cs
@@ -9,7 +9,56 @@ namespace while_satsen
     class Program
     {
         static void Main(string[] args)
-        {
+        {/*
+            //Övning 1.1
+            int x = 0;
+            while (x<100) {
+                x++;
+                Console.WriteLine(x);
+            }
+            Console.ReadKey();
+
+            //Övning 1.2
+
+            Console.WriteLine("Mata in ditt lösenord: ");
+            var password = Console.ReadLine();
+            while (password != "linnea")
+            {
+                Console.WriteLine("Fel! Mata in igen:");
+                password = Console.ReadLine();
+            }
+            
+            //Övning 2.1
+
+            int i = 1;
+            while (i <= 100)
+            {
+                i++;
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+            
+            //Övning 2.2
+
+            int i = 10;
+
+            while (i >= 1)
+            {
+                i--;
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+            
+            //Övning 2.3
+
+            while (true)
+                Console.WriteLine("Hjälp!");
+      */
+            //Övning 2.4
+
+
+
         }
     }
 }
